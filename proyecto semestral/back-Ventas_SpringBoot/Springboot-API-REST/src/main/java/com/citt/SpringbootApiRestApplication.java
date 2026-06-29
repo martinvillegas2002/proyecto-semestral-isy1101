e95d33148708e6c90e93e6da67cb8e2aaa6529ca
Martin Aaron Villegas Roa <ma.villegasr@duocuc.cl>
2026-06-29 14:24:55 -0400

    Refactor: eliminar endpoint de health innecesario

--- proyecto semestral/back-Ventas_SpringBoot/Springboot-API-REST/src/main/java/com/citt/SpringbootApiRestApplication.java
+++ proyecto semestral/back-Ventas_SpringBoot/Springboot-API-REST/src/main/java/com/citt/SpringbootApiRestApplication.java
@@ -2,20 +2,11 @@ package com.citt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping; // <--- Importa esto
-import org.springframework.web.bind.annotation.RestController; // <--- Importa esto
 
 @SpringBootApplication
-@RestController // <--- Marca esta clase como controlador
 public class SpringbootApiRestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApiRestApplication.class, args);
-    }
-
-    // El endpoint de salud para AWS
-    @GetMapping("/health")
-    public String health() {
-        return "OK";
     }
 }
